0c2e21e841833f88139dfd654da12104faf82002
zenladysh <52216064+zenladysh@users.noreply.github.com>
2019-07-02 09:14:05 +0300

    Task6_49(a,b)

--- Task6_49.java
+++ Task6_49.java
@@ -4,24 +4,56 @@ import java.util.Scanner;
 
 public class Task6_49 {
     public static void main(String[] args) {
-        
-        System.out.println("ВВедите число");
+
+        System.out.println("ВВедите вариант задачи(a или b)");
         Scanner in = new Scanner(System.in);
+        String varEx = in.next();
+
+        System.out.println("ВВедите число");
+
         int num = in.nextInt();
-        int j = 0;
-        for (int i = 1; i > 0; i++) {
-            if (num % 10 == 3) {
-                j++;
+        int numOut = num;
+        switch (varEx) {
+            case "a":
 
-                num = num / 10;
-            } else {
-                num = num / 10;
+                int j = 0;
+                for (int i = 1; i > 0; i++) {
+                    if (num % 10 == 3) {
+                        j++;
 
-            }
-            if (num == 0)
+                        num = num / 10;
+                    } else {
+                        num = num / 10;
+
+                    }
+                    if (num == 0)
+                        break;
+                }
+
+                System.out.println("Число "+numOut+" содержит цифру 3: " + j + " раз(а)");
+                break;
+            case "b":
+                int k = 0;
+                int m = 0;
+                for (int n = 1; n > 0; n++) {
+                    if (num % 10 == 2) {
+                        k++;
+
+                        num = num / 10;
+                    } else if (num % 10 == 5) {
+                        m++;
+                        num = num / 10;
+
+                    } else {
+                        num = num / 10;
+                    }
+                    if (num == 0)
+                        break;
+                }
+
+                System.out.println("Число "+numOut+" содержит цифру 2: " + k + " раз(а)");
+                System.out.println("Число "+numOut+" содержит цифру 5: " + m + " раз(а)");
                 break;
         }
-
-        System.out.println("Число содержит цифру 3: " + j + " раз(а)");
     }
 }
