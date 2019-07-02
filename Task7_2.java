ea5fbb3253594014de381152595bd048135bea5d
zenladysh <52216064+zenladysh@users.noreply.github.com>
2019-07-02 09:29:20 +0300

    Task7_2

---
+++ Task7_2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Task7_2 {
+    public static void main(String[] args) {
+
+        int a=3;
+        int b=234;
+        int c=5;
+        for (int i = a; i <= b; i++) {
+            if (i % c == 0)
+                System.out.println(i);
+        }
+
+    }
+}
