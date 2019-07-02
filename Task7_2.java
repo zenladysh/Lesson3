0ccc6cccd89cb1d7ab837e59bfa77058d6d5735a
zenladysh <52216064+zenladysh@users.noreply.github.com>
2019-07-02 09:37:52 +0300

    Task7_2
    Добавлен ввод данных с консоли

--- Task7_2.java
+++ Task7_2.java
@@ -1,13 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Task7_2 {
     public static void main(String[] args) {
 
-        int a=3;
-        int b=234;
-        int c=5;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите a");
+        int a=in.nextInt();
+        System.out.println("Введите b");
+        int b=in.nextInt();
+        System.out.println("Введите c");
+        int c=in.nextInt();
+        System.out.println("Числа кратные "+c+":");
         for (int i = a; i <= b; i++) {
             if (i % c == 0)
+
                 System.out.println(i);
         }
 
